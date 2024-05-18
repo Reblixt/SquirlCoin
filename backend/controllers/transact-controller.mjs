75ad5d26731e07f746a19fc1b9e0343bf3cef893
Reblixt <carl@klofverskjold.se>
2024-05-18 14:36:25 +0200

    added data variable in createTransaction

--- backend/controllers/transact-controller.mjs
+++ backend/controllers/transact-controller.mjs
@@ -2,7 +2,11 @@ import { blockchain, redisServer } from "../server.mjs";
 
 export const createTransaction = (req, res, next) => {
   const { sender, recipient, amount } = req.body;
-  const transaction = { sender, recipient, amount };
+  let { data } = req.body;
+  if (!data) {
+    data = "No additional data provided for this transaction";
+  }
+  const transaction = { sender, recipient, amount, data };
   blockchain.addTransaction(transaction);
 
   res.status(201).json({ success: true, statusCode: 201, data: transaction });
