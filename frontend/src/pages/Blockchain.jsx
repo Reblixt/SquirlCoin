7adcb93dd6034fc7ba4f191c670d156b369c0bff
Rasmus <rasmus.wersall@gmail.com>
2024-05-18 16:57:15 +0200

    :construction: hooks

--- frontend/src/pages/Blockchain.jsx
+++ frontend/src/pages/Blockchain.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useBlockchain } from '../hooks/useBlockchain';
 
 export const Blockchain = () => {
+
+  const { blockchain, loading, error } = useBlockchain();
+
   return (
     <div>
       <h2>Squirl Chain</h2>
       <section>
-        Här visas hela blockkedjan:
+      {loading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+        {blockchain.map((block) => (
+          <div className="block-wrapper" key={block.blockIndex}>
+            <div>Blockindex: {block.blockIndex}</div>
+            <div>Previous Blockhash: {block.previousBlockHash}</div>
+            <div>Current Blockchain: {block.currentBlockHash}</div>
+            <div>Timestamp: {block.timestamp}</div>
+          </div>
+        ))}
       </section>
     </div>
   )
