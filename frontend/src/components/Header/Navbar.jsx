7117cf986ce1748207dd373b3656acdf0ca9447b
Sanjin Dumisic <codebatine@proton.me>
2024-05-19 09:04:29 +0200

    jsut began squirling up some css

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../content/img/sqrrrl-logo.png";
 
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-  <nav>
-    <ul>
-      <li><NavLink to={"/"}>Squirl Coin</NavLink></li>
-      <li><NavLink to={"/transact"}>Nut trading</NavLink></li>
-      <li><NavLink to={"/blockchain"}>Blockchain</NavLink></li>
-      <li><NavLink to={"/block"}>Block</NavLink></li>
-    </ul>
-  </nav>
+    <nav>
+      <div id="menu-icon" onClick={toggleMenu} className={isOpen ? "open" : ""}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <NavLink to={"/"}>
+            <img
+              src={logo}
+              alt="Logo"
+              className="logo"
+              onClick={() => setIsOpen(false)}
+            />
+          </NavLink>
+        </li>
+        <li><NavLink to={"/"} onClick={toggleMenu}>Squirl Coin</NavLink></li>
+        <li><NavLink to={"/transact"} onClick={toggleMenu}>Nut trading</NavLink></li>
+        <li><NavLink to={"/blockchain"} onClick={toggleMenu}>Blockchain</NavLink></li>
+        <li><NavLink to={"/block"} onClick={toggleMenu}>Block</NavLink></li>
+      </ul>
+    </nav>
   )
 }
