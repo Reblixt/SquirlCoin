3af1e3c027bb1bcacbda6a93de168a57f1bea140
Sanjin Dumisic <codebatine@proton.me>
2024-05-19 10:21:00 +0200

    logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import logo from '../content/img/sqrrrl-logo.png';
+
 export const Home = () => {
   return (
-    <div>
-      <h2>SQUIRL COIN</h2>
-      <article>Send, receive and explore</article>
+    <div className="home-container">
+      <img src={logo} alt="Logo" className="mobile-logo" />
+      <h1>SQUIRL COIN</h1>
+      <article>Nut trading made easy!</article>
     </div>
   )
 }
